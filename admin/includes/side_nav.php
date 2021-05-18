b14de1a3629081e5ea4ba5cf63a10227bcaa221a
Ben Ringia <benringia@gmail.com>
2021-05-18 16:48:21 +0800

    set up files /links

---
+++ admin/includes/side_nav.php
@@ -0,0 +1,29 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>
+        </li>
+        <li>
+            <a href="upload.php"><i class="fa fa-fw fa-table"></i> Upload</a>
+        </li>
+        <li>
+            <a href="comments.php"><i class="fa fa-fw fa-edit"></i> Comments</a>
+        </li>
+        
+        <!-- <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="demo" class="collapse">
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+            </ul>
+        </li> -->
+       
+    </ul>
+</div>
