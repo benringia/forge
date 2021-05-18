c673bd9748d0cd918a9471173506c7fed0bd9c62
Ben Ringia <benringia@gmail.com>
2021-05-18 23:31:33 +0800

    update css

--- admin/photos.php
+++ admin/photos.php
@@ -21,10 +21,7 @@
             <!-- Page Heading -->
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">
-                        Photos
-                        <small>Subheading</small>
-                    </h1>
+                    
                     <ol class="breadcrumb">
                         <li>
                             <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
@@ -33,6 +30,10 @@
                             <i class="fa fa-file"></i> Blank Page
                         </li>
                     </ol>
+                    <h1 class="page-header">
+                        Photos
+                        <small>Subheading</small>
+                    </h1>
                 </div>
             </div>
             <!-- /.row -->
